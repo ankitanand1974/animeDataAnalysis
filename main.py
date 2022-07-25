24c3044e0222b26808c0f7a2b194197fa3d35da3
Ankit <ankitanand1974@gmail.com>
2022-07-25 20:55:34 +0530

    analysis done

---
+++ main.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+print("      ___________________  ")
+print("  -->|   Data Analysis   |<---")
+print("     |     on Anime      |    ")
+print("     |___________________|     ")
+dataset = pd.read_csv("anime.csv")
+
+print("WELCOME TO Anime Data Analysis\n")  
+  
+while True:  
+    print("MENU")  
+    print("1. Graph on type of anime")  
+    print("2. Episodes count of different anime")  
+    print("3. Rating  of different anime")  
+    print("4. Active members")  
+    print("5. Top 5 genre")  
+    users_choice = int(input("\nEnter your Choice: "))  
+  
+    if users_choice == 1:  
+          dataset['type'].value_counts().plot(kind='bar' , figsize=(12,12))
+        
+  
+    elif users_choice == 2:  
+        dataset['episodes'].value_counts().plot(kind='bar' , figsize=(12,12))
+       
+
+    elif users_choice == 3:  
+        dataset['rating'].value_counts().plot(kind='bar' , figsize=(12,12))
+
+  
+    elif users_choice == 4:  
+        dataset['members'].value_counts().plot(kind='bar' , figsize=(12,12))
+
+
+    elif users_choice == 5:  
+        genreList=[]
+        genre_dict = {}
+        for key, value in genre_dict.items():
+            temp = [key,value]
+            genreList.append(temp)
+
+        genre_df=pd.DataFrame(genreList, columns = ["Genre", "Count"]) 
+        genre_df.sort_values("Count").tail(5)
+        break  
+      
+    else:  
+        print( "Please enter a valid Input from the list")  
